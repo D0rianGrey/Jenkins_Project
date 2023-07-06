81c834315609b67491587613b5d067cb995233d8
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-06 18:09:56 +0200

    Add before each

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,16 +1,23 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTests {
-    @Test
-    void test() {
+
+    @BeforeEach
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         Configuration.browserCapabilities = options;
+        Configuration.browser = "chrome";
+    }
+
+    @Test
+    void test() {
         open("https://www.google.com");
     }
 }
