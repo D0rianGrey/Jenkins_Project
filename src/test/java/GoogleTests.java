e976bc172bd533cc72d9f88b656a2e6ca4ce160c
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-06 18:07:00 +0200

    Add headless flag

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,12 +1,16 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTests {
     @Test
     void test() {
-        Configuration.headless = true;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        Configuration.browserCapabilities = options;
         open("https://www.google.com");
     }
 }
