d2287e745491c541333611b0ddbad6183168da38
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-06 20:05:55 +0200

    Add arguments

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,9 +10,12 @@ public class GoogleTests {
 
     @BeforeEach
     void setUp() {
+        WebDriverManager.chromedriver().driverVersion("114").setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         Configuration.browserCapabilities = options;
         Configuration.browser = "chrome";
     }
