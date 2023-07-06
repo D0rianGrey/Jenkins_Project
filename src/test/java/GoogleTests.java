cc2f77d0f94bac63eb9cd0b145e253b5abf4be00
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-06 20:10:20 +0200

    Add arguments

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -3,6 +3,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,14 +11,19 @@ public class GoogleTests {
 
     @BeforeEach
     void setUp() {
-        WebDriverManager.chromedriver().setup();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        Configuration.browserCapabilities = options;
-        Configuration.browser = "chrome";
+        options.addArguments("--start-maximized");
+        options.addArguments("--no-sandbox");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        capabilities.setAcceptInsecureCerts(true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.screenshots = true;
+        Configuration.webdriverLogsEnabled = true;
+        Configuration.headless = true;
     }
 
     @Test
