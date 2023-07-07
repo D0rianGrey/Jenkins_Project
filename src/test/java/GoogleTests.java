f888a8a8a24626c364d6b9accb393d6382967c2e
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-07 11:35:08 +0200

    Delete arguments for selenide and add gradlew

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -12,20 +12,20 @@ public class GoogleTests {
 
     @BeforeEach
     void setUp() {
-        WebDriverManager.chromedriver().driverVersion("114").setup();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--start-maximized");
-        options.addArguments("--no-sandbox");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        capabilities.setAcceptInsecureCerts(true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.screenshots = true;
-        Configuration.webdriverLogsEnabled = true;
-        Configuration.headless = true;
+//        WebDriverManager.chromedriver().driverVersion("114").setup();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--start-maximized");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("disable-infobars");
+//        options.addArguments("--disable-extensions");
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        capabilities.setAcceptInsecureCerts(true);
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.screenshots = true;
+//        Configuration.webdriverLogsEnabled = true;
+//        Configuration.headless = true;
     }
 
     @Test
