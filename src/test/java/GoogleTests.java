cbfa79ae46c200491071c9daa808655da7c1371d
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-09 13:17:33 +0200

    Add headless flag

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,10 +1,6 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,6 +8,7 @@ public class GoogleTests {
 
     @BeforeEach
     void setUp() {
+        Configuration.headless = true;
 //        WebDriverManager.chromedriver().driverVersion("114").setup();
 //        DesiredCapabilities capabilities = new DesiredCapabilities();
 //        ChromeOptions options = new ChromeOptions();
