03bf270b12815d9e5c103ebb47b54b85a8ac10b9
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-09 13:35:04 +0200

    Add chrome path

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,9 +1,5 @@
-import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,20 +7,21 @@ public class GoogleTests {
 
     @BeforeEach
     void setUp() {
-        WebDriverManager.chromedriver().driverVersion("114").setup();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--start-maximized");
-        options.addArguments("--no-sandbox");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        capabilities.setAcceptInsecureCerts(true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.screenshots = true;
-        Configuration.webdriverLogsEnabled = true;
-        Configuration.headless = true;
+//        WebDriverManager.chromedriver().driverVersion("114").setup();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--start-maximized");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("disable-infobars");
+//        options.addArguments("--disable-extensions");
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        capabilities.setAcceptInsecureCerts(true);
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.screenshots = true;
+//        Configuration.webdriverLogsEnabled = true;
+//        Configuration.headless = true;
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
     }
 
     @Test
