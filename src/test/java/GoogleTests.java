c2680f6742868a31858411becdc9242897e860ed
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-07-09 13:59:46 +0200

    Add chrome path 8

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,35 +1,48 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTests {
 
-    @BeforeEach
-    void setUp() {
-        String chromeDriverPath = System.getenv("CHROMEDRIVER_PATH");
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--start-maximized");
-        options.addArguments("--no-sandbox");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        capabilities.setAcceptInsecureCerts(true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.screenshots = true;
-        Configuration.webdriverLogsEnabled = true;
-        Configuration.headless = true;
-        Configuration.browser = "chrome";
-    }
+//    @BeforeEach
+//    void setUp() {
+//        String chromeDriverPath = System.getenv("CHROMEDRIVER_PATH");
+//        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+////        DesiredCapabilities capabilities = new DesiredCapabilities();
+////        ChromeOptions options = new ChromeOptions();
+////        options.addArguments("--disable-dev-shm-usage");
+////        options.addArguments("--start-maximized");
+////        options.addArguments("--no-sandbox");
+////        options.addArguments("disable-infobars");
+////        options.addArguments("--disable-extensions");
+////        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+////        capabilities.setAcceptInsecureCerts(true);
+////        Configuration.browserCapabilities = capabilities;
+////        Configuration.screenshots = true;
+////        Configuration.webdriverLogsEnabled = true;
+////        Configuration.headless = true;
+////        Configuration.browser = "chrome";
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArgument("--disable-gpu");
+//        options.addArgument("--no-sandbox");
+//        options.setBinary(chromeDriverPath);
+//        ChromeDriver driver = new ChromeDriver(options);
+//    }
+//
+//    @Test
+//    void test1() {
+////        open("https://www.google.com");
+//
+//    }
 
     @Test
-    void test1() {
-        open("https://www.google.com");
+    void test() {
+        String chromeDriverPath = System.getenv("CHROMEDRIVER_PATH");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.setBinary(chromeDriverPath);
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
     }
 }
